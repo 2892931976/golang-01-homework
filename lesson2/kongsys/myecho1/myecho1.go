2a7c1f210c9478f7aed9ee166262ad0067ffdc01
kongsys <kongsys@163.com>
2017-06-10 15:26:57 +0800

    delete some code

--- lesson2/kongsys/myecho1/myecho1.go
+++ lesson2/kongsys/myecho1/myecho1.go
@@ -11,9 +11,8 @@ var nline = flag.Bool("n", false, "new line")
 
 func main() {
   flag.Parse()
-  if *nline {
-    fmt.Println(strings.Join(flag.Args(), *sep))
-  } else {
     fmt.Print(strings.Join(flag.Args(), *sep))
+  if *nline {
+    fmt.Println()
   }
 }
