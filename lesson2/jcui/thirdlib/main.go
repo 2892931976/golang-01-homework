364b9a320e17f2192640f0482d10c9b2aa93ce44
cui6522123@gmail.com <cui@6522123>
2017-06-10 12:56:31 +0800

    lesson2

--- lesson2/jcui/thirdlib/main.go
+++ lesson2/jcui/thirdlib/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/51reboot/golang-01-homework/lesson2/jcui/thirdlib/math"
+	"github.com/51reboot/golang-01-homework/lesson2/jcui/thirdlib/test_math"
 )
 
 func main() {
-	fmt.Println(math.Add(2, 3))
+	fmt.Println(test_math.Add(2, 3))
+	fmt.Println(test_math.Mult(2, 3))
 
 }
