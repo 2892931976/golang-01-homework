fae2275313aa58fcae7971431b6c0a96903ce0de
luojiyin <luojiyin@hotmail.com>
2017-06-13 23:16:15 +0800

    在go中一个内部声明可能屏蔽外部的声明，有不同的语法域   如if for等控制流语句

--- lesson2/luojiyin/var.go
+++ lesson2/luojiyin/var.go
@@ -14,10 +14,10 @@ func main() {
 	localFunc()    //localFunc()是被调用的函数，是读取不到main()局部变量,只能读取到全局变量
 	fmt.Println(x) //
 	if true {
-		x := 100 //更新局部变量的值为100
-		fmt.Println(x)
+		x := 100 //更新局部变量的值为100 他的作用域是if或者for 这样的控制流内部
+		fmt.Println(x)//if 外的局部变量x 的就恢复成 1
 	}
 
 	localFunc()    //localFunc()是被调用的函数，是读取不到main()局部变量,只能读取到全局变量
-	fmt.Println(x) //为什么是1  不是100 ？？
+	fmt.Println(x) //x为100，是在if控制语句中，出来了就恢复成1
 }
