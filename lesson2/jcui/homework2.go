757584a4396786bd6c79d148f8c050856323fb75
cuijia <cui6522123@gmail.com>
2017-06-16 18:31:08 +0800

    说明变量作用域

--- lesson2/jcui/homework2.go
+++ lesson2/jcui/homework2.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Println(x) //这里调用的是局部变量x，输出为1
 	if true {
 		x := 100       //在if语段中定义新的局部变量x=100
-		fmt.Println(x) //输出调用x为100
+		fmt.Println(x) //输出调用x为100,if结束,变量失去作用
 	}
 
 	localFunc()    //调用全局变量x，输出为200
