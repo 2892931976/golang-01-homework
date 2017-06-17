9f0ffdca1c40b5655dfb10ddb9c53f52876d444e
kongsys <kongsys@163.com>
2017-06-17 17:49:25 +0800

    change times table output order.

--- lesson3/kongsys/9_times_9_table.go
+++ lesson3/kongsys/9_times_9_table.go
@@ -13,7 +13,7 @@ func main() {
 	}
 	for i := 1; i < 10; i++ {
 		for j := 1; j <= i; j++ {
-			fmt.Fprintf(f, "%dx%d=%-2d ", i, j, i*j)
+			fmt.Fprintf(f, "%dx%d=%-2d ", j, i, i*j)
 		}
 		fmt.Fprintf(f, "\n")
 	}
