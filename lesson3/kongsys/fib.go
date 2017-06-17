b6076564b0368f8000ef4f09c92cd2c4a983bbc0
kongsys <kongsys@163.com>
2017-06-17 17:46:44 +0800

    gofmt fib.go

--- lesson3/kongsys/fib.go
+++ lesson3/kongsys/fib.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-  var sum int = 0
-  var i1, i2 int = 0, 1
-  for i2 < 100 {
-    i1, i2 = i2, i1 + i2 
-    sum += i1
-  }
-  fmt.Println(sum)
+	var sum int = 0
+	var i1, i2 int = 0, 1
+	for i2 < 100 {
+		i1, i2 = i2, i1+i2
+		sum += i1
+	}
+	fmt.Println(sum)
 
 }
