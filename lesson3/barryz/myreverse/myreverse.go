d64cc1fe228385e35e6ab69a65b17ac770b715b5
barryz <barryzxb@gmail.com>
2017-06-19 19:22:30 +0800

    add reverse function

--- lesson3/barryz/myreverse/myreverse.go
+++ lesson3/barryz/myreverse/myreverse.go
@@ -5,16 +5,31 @@ import (
 	"os"
 )
 
+func reverseOne(n []rune) string {
+	sz := len(n)
+	for i, j := 0, sz-1; i < j; i, j = i+1, j-1 {
+		n[i], n[j] = n[j], n[i]
+	}
+
+	return string(n)
+}
+
+func reverseTwo(n []rune) string {
+	sz := len(n)
+	for i := 0; i < sz/2; i++ {
+		mIndex := sz - i - 1
+		n[i], n[mIndex] = n[mIndex], n[i]
+	}
+
+	return string(n)
+}
+
 func main() {
 	strs := os.Args[1]
 	nbyte := []rune(strs)
-	sz := len(nbyte)
 
-	for i, j := 0, sz-1; i < j; i, j = i+1, j-1 {
-		nbyte[i], nbyte[j] = nbyte[j], nbyte[i]
-	}
-
-	fmt.Println(string(nbyte))
+	fmt.Printf("Reverse Once: %s\n", reverseOne(nbyte))
+	fmt.Printf("Reverse Twice: %s\n", reverseTwo(nbyte))
 
 	/* output:
 	$go run myreverse.go ABCDEFG
