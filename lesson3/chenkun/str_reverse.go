9ab0affcc6dca3899744b2a94631f4101e11d3f7
陈琨 <chenkun1998@163.com>
2017-06-21 00:53:41 +0800

    change note

--- lesson3/chenkun/str_reverse.go
+++ lesson3/chenkun/str_reverse.go
@@ -9,7 +9,7 @@ var retstr string
 
 func main() {
 	str1 := os.Args[1]
-	var array = []byte(str1)               // str1转换成切片
+	var array = []byte(str1)               // str1转换
 	for i := len(array) - 1; i >= 0; i-- { // 翻转
 		retstr = retstr + string(array[i])
 	}
