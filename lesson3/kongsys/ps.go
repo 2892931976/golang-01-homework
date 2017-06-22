7f0789d2eaec23044ea7db5adfdaf591fc5c99e4
kongsys <kongsys@163.com>
2017-06-22 13:51:35 +0800

    simple ps by golang

---
+++ lesson3/kongsys/ps.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	dirs, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		fmt.Println("read dir error.")
+		os.Exit(1)
+	}
+	for _, d := range dirs {
+		if d.IsDir() {
+			if pid, err := strconv.Atoi(d.Name()); err == nil {
+				pathname := fmt.Sprintf("/proc/%d/stat", pid)
+				f, err := os.Open(pathname)
+				if err != nil {
+					continue
+				}
+				r := bufio.NewReader(f)
+				line, _ := r.ReadString('\n')
+				statslice := strings.Split(line, " ")
+				tempL := len(statslice[1])
+				app := statslice[1][1 : tempL-1] // delete '(' and  ')', origin app like "(sshd)"
+				fmt.Println(app, d.Name())
+			}
+		}
+	}
+}
