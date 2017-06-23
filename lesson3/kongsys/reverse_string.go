170b9322b8c8fd021b74106901aa3d45a9ab041b
kongsys <kongsys@163.com>
2017-06-23 14:05:55 +0800

    add reverse lib demo

---
+++ lesson3/kongsys/reverse_string.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "github.com/51reboot/golang-01-homework/lesson3/kongsys/reverse_lib"
+)
+
+func main() {
+    var input string = "你好世界，just demo"
+    // var input string = "just demo"
+    fmt.Println("origin :", input)
+    re_input := reverse.Reverse(input)
+    fmt.Println("reverse :", re_input)
+}
+
