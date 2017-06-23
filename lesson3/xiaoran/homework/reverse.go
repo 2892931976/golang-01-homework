f0e7a5d6a36f68c0f8a8c6e6d8a1bbd2240456b0
bruce1230 <bruce1230@163.com>
2017-06-23 20:21:36 +0800

    reverse.go

--- lesson3/xiaoran/homework/reverse.go
+++ lesson3/xiaoran/homework/reverse.go
@@ -11,8 +11,8 @@ func main() {
 	str := os.Args[1]
 	var array = []byte(str)
 	fmt.Printf("翻转前: %s\n", str)
-	for i := len(array) - 1; i >= 0; i-- {
-		result = result + string(array[i])
+	for i := 1; len(array)-i >= 0; i++ {
+		result = result + string(array[len(array)-i])
 	}
 	fmt.Printf("翻转后: %s\n", result)
 }
