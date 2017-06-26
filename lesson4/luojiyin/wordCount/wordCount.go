7e02f96428d6dda2269484c0774a07fa6d707b1f
luojiyin1987 <luojiyin@hotmail.com>
2017-06-26 15:40:37 +0800

    sort wordCount order by asc

--- lesson4/luojiyin/wordCount/wordCount.go
+++ lesson4/luojiyin/wordCount/wordCount.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strings"
 )
 
@@ -12,11 +13,28 @@ func main() {
 		return
 	}
 	temp := strings.Fields(string(fs))
-	fmt.Printf("Fields are:%q", temp)
-	for _, word := range temp {
-		fmt.Println(word)
+	//fmt.Printf("Fields are:%q", temp)
+	//for _, word := range temp {
+	//	fmt.Println(word)
+	//}
+	//fmt.Println(countWord(temp))
+	temp1 := countWord(temp)
+	for k, v := range temp1 {
+		fmt.Println(k, v)
 	}
-	fmt.Println(countWord(temp))
+	pl := make(PairList, len(temp1))
+	i := 0
+	for k, v := range temp1 {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	fmt.Println("-------------------------")
+	fmt.Println("after sort")
+	fmt.Println("--------------------------")
+	sort.Sort(pl)
+	for _, k := range pl {
+		fmt.Printf("%s %d\n", k.Key, k.Value)
+	}
 }
 
 func countWord(words []string) map[string]int {
@@ -28,3 +46,14 @@ func countWord(words []string) map[string]int {
 	return wordCounts
 
 }
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
