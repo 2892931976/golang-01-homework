99443a9efba555b714c0514951e4641038a82577
jungle85gopy <jungle85gopy@gmail.com>
2017-06-27 23:12:13 +0800

    modify lesson4/jungle85gopy/stuServer/main.go finish save function
    1) add saveStu(), checkFileExist(), checkOverride() for save
    2) format struct define
    3) do nothing when blank cmd, not call usage()

--- lesson4/jungle85gopy/stuServer/main.go
+++ lesson4/jungle85gopy/stuServer/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 )
 
+// Student struct for student info
 type Student struct {
-	Id   int
-	Name string
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 var students = make(map[string]Student)
@@ -26,7 +29,6 @@ func main() {
 		if cmd == "exit" {
 			break
 		}
-		// fmt.Println("input:", line, "len:", len(line))
 		switch cmd {
 		case "add":
 			addStu(name, id)
@@ -36,14 +38,64 @@ func main() {
 			loadStu(name)
 		case "save":
 			saveStu(name)
+		case "":
+			continue
 		default:
 			usage()
 		}
+		line, cmd = "", ""
 	}
 }
 
 func saveStu(name string) {
-	fmt.Println("params:", name)
+	var err error
+	var fd *os.File
+
+	// file existed and override
+	if checkFileExist(name) {
+		if !checkOverride(name) {
+			fmt.Println(" + cancel save to", name)
+			return
+		}
+		if fd, err = os.OpenFile(name, os.O_RDWR|os.O_TRUNC, 0644); err != nil {
+			log.Fatal("open file error of %s", name)
+		}
+	}
+	// save to new file
+	if fd, err = os.Create(name); err != nil {
+		log.Fatal("open new file error of %s", name)
+	}
+	if buf, err := json.Marshal(students); err != nil {
+		log.Fatal("marshal stu info error")
+	} else if _, err := fd.Write(buf); err == nil {
+		fmt.Println(" + save success")
+	} else {
+		log.Fatal("save error")
+	}
+}
+
+// check file f exist or not
+func checkFileExist(f string) bool {
+	if _, err := os.Stat(f); err == nil {
+		return true
+	} else if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+// check override the give file name
+func checkOverride(f string) bool {
+	var s string
+	for {
+		fmt.Printf(" + override file: %s, y or n? :", f)
+		fmt.Scanf("%s", &s)
+		if string(s[0]) == "y" || string(s[0]) == "Y" {
+			return true
+		} else if string(s[0]) == "n" || string(s[0]) == "N" {
+			return false
+		}
+	}
 }
 
 func loadStu(name string) {
@@ -51,29 +103,29 @@ func loadStu(name string) {
 }
 
 func addStu(name string, id int) {
-	// fmt.Println("-- params:", name, id)
 	if _, ok := students[name]; ok {
-		fmt.Println("duplicated name:", name)
+		fmt.Println(" + duplicated name:", name)
 		return
 	}
-	students[name] = Student{Id: id, Name: name}
+	students[name] = Student{ID: id, Name: name}
 }
 
 func listStu() {
 	if len(students) == 0 {
+		fmt.Println(" + no student info here")
 		return
 	}
-	fmt.Println("Id\tName:")
+	fmt.Println(" + Id\tName:")
 	for _, val := range students {
-		fmt.Printf("%d\t%s\n", val.Id, val.Name)
+		fmt.Printf(" + %d\t%s\n", val.ID, val.Name)
 	}
 }
 
 func usage() {
-	fmt.Println("cli usage:")
-	fmt.Println("    add name id -- add student info")
-	fmt.Println("    list \t-- list student info")
-	fmt.Println("    load file \t-- load student from file")
-	fmt.Println("    save file \t-- save student info file")
-	fmt.Println("    exit \t-- exit the cli")
+	fmt.Println(" + cli usage:")
+	fmt.Println(" + add name id -- add student info")
+	fmt.Println(" + list \t-- list student info")
+	fmt.Println(" + load file \t-- load student from file")
+	fmt.Println(" + save file \t-- save student info file")
+	fmt.Println(" + exit \t-- exit the cli")
 }
