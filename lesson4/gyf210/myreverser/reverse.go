2ba40362d226a7e644b1a3668038497b2eae6899
gyf210 <316036643@qq.com>
2017-06-29 17:50:30 +0800

    modify zuoye

--- lesson4/gyf210/myreverser/reverse.go
+++ lesson4/gyf210/myreverser/reverse.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"strings"
 )
 
-func myslice(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+func myslice(s []int, n int) error {
+	if n < 0 {
+		return errors.New("截断长度小于0")
 	}
+
+	if n == 0 || n >= len(s) {
+		return nil
+	}
+
+	var t []int
+	t = append(t, s[n:]...)
+	t = append(t, s[:n]...)
+	copy(s, t)
+	return nil
 }
 
 func mystring(str string) string {
@@ -22,9 +34,10 @@ func mystring(str string) string {
 func main() {
 	a := []int{2, 3, 5, 7, 11}
 	fmt.Println(a)
-	myslice(a[:2])
-	myslice(a[2:])
-	myslice(a)
+	err := myslice(a, 2)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(a)
 	fmt.Println()
 
