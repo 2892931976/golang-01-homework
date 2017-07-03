ff5a38ba904e6f5b2ea4028a707e90cdbe1257d4
zhengyansheng <zhengyansheng@vipkid.com.cn>
2017-07-03 17:21:59 +0800

    add shell.go

---
+++ lesson5/zhengyscn/shell.go
@@ -0,0 +1,67 @@
+/*
+	example:
+		df
+		df -h
+		ls | grep f
+		cat /etc/passwd | grep apache
+
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	host, _ := os.Hostname()
+	prompt := fmt.Sprintf("[python27env@%s]$ ", host)
+	r := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		if !r.Scan() {
+			break
+		}
+		line := r.Text()
+		if len(line) == 0 {
+			continue
+		}
+
+		if !strings.Contains(line, "|") {
+			args := strings.Fields(line)
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		} else {
+			cmds := strings.Split(line, "|")
+			s1 := strings.Fields(cmds[0])
+			s2 := strings.Fields(cmds[1])
+
+			r, w := io.Pipe()
+			defer func() {
+				err := recover()
+				fmt.Println(err)
+			}()
+			cmd1 := exec.Command(s1[0], s1[1:]...)
+			cmd2 := exec.Command(s2[0], s2[1:]...)
+			cmd1.Stdin = os.Stdin
+			cmd1.Stdout = w
+			cmd2.Stdin = r
+			cmd2.Stdout = os.Stdout
+			cmd1.Start()
+			cmd2.Start()
+
+			cmd1.Wait()
+		}
+	}
+}
