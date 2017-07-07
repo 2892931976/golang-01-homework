246b96c09073c79661c707e9d8ec23db2a38e4ae
rzhao2333 <543551194@qq.com>
2017-07-07 17:14:36 +0800

    mypipe.go

--- lesson5/zhaoyuanhai/myPipe.go
+++ lesson5/zhaoyuanhai/myPipe.go
@@ -40,6 +40,19 @@ func main() {
 		if len(line) == 0 {
 			continue
 		}
-		mypipe(line)
+		if !strings.Contains(line, "|") {
+			args := strings.Fields(line)
+			cmd := exec.Command(args[0], args[1:]...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+
+		} else {
+			mypipe(line)
+		}
 	}
 }
