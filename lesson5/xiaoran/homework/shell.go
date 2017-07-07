180cf7aec2d9ad921520ed01d77f5b98ae0899b4
bruce1230 <bruce1230@163.com>
2017-07-07 18:22:31 +0800

    homework

---
+++ lesson5/xiaoran/homework/shell.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	host, _ := os.Hostname()
+	prompt := fmt.Sprintf("[xiao@%s]$ ", host)
+	r := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print(prompt)
+		if !r.Scan() {
+			break
+		}
+		line := r.Text()
+		if len(line) == 0 {
+			continue
+		}
+
+		//对输入的命令切割
+		cmds := strings.Split(line, "|")
+		s1 := strings.Fields(cmds[0])
+		s2 := strings.Fields(cmds[1])
+
+		r, w := io.Pipe()
+		cmd1 := exec.Command(s1[0], s1[1:]...)
+		cmd2 := exec.Command(s2[0], s2[1:]...)
+
+		cmd1.Stdin = os.Stdin
+		cmd1.Stdout = w
+		cmd2.Stdin = r
+		cmd2.Stdout = os.Stdout
+		cmd1.Start()
+		cmd2.Start()
+
+		cmd1.Wait()
+	}
+}
