585d1076025c3820e243e8ca711f8f6e757fb46a
wujianjian <wujianjian@example.com>
2017-07-07 21:04:15 +0800

    homework

--- lesson5/wujianjian/homework/shell.go
+++ lesson5/wujianjian/homework/shell.go
@@ -9,6 +9,13 @@ import (
 	"strings"
 )
 
+/*
+
+命令1：ls -l | grep go
+命令2：cat shell.go | grep main
+
+*/
+
 func main() {
 	host, _ := os.Hostname()
 	prompt := fmt.Sprintf("[wujianjian@%s]$ ", host)
@@ -25,8 +32,8 @@ func main() {
 		cmds := strings.Split(line, "|")
 		s1 := strings.Fields(cmds[0])
 		s2 := strings.Fields(cmds[1])
-		fmt.Println(s1)
-		fmt.Println(s2)
+		//fmt.Println(s1)
+		//fmt.Println(s2)
 
 		r, w := io.Pipe()
 		cmd1 := exec.Command(s1[0], s1[1:]...)
