aff04c9596107e40f5c0b8a4c0a35ab1b0bcc2c9
root <root@localhost.(none)>
2017-07-07 10:55:59 +0800

    homework5

---
+++ lesson5/xuezhaomeng/shell.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	host, _ := os.Hostname()
+
+	prompt := fmt.Sprintf("[xuezhaomeng@%v]$ ", host)
+	r := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		if !r.Scan() {
+			break
+		}
+		line := r.Text()
+
+		if len(line) == 0 {
+			continue
+		}
+		if strings.Contains(line, "|") {
+			cmds := strings.Split(line, "|")
+			s1 := strings.Fields(cmds[0])
+			s2 := strings.Fields(cmds[1])
+			cmd1 := exec.Command(s1[0], s1[1:]...)
+			cmd2 := exec.Command(s2[0], s2[1:]...)
+			out, _ := cmd1.StdoutPipe()
+			cmd2.Stdin = out
+			cmd2.Stdout = os.Stdout
+			cmd1.Start()
+			cmd2.Start()
+			cmd1.Wait()
+			//不匹配的结果的错误提醒也输出,暂时不会解决
+			//bug1: 命令1 的结果也进行了输出
+		}
+		args := strings.Fields(line)
+		cmd := exec.Command(args[0], args[1:]...)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+}
