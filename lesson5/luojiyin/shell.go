655bccd2f628b12ecaa1c830f2acae8b0de31ed3
luojiyin1987 <luojiyin@hotmail.com>
2017-07-07 11:12:42 +0800

    can not run second cmd

--- lesson5/luojiyin/shell.go
+++ lesson5/luojiyin/shell.go
@@ -24,14 +24,27 @@ func main() {
 		if len(line) == 0 {
 			continue
 		}
-		args := strings.Fields(line)
-		cmd := exec.Command(args[0], args[1:]...)
+		//args := strings.Fields(line)
+		args := strings.Split(line, "|")
+		for _, arg := range args {
+			fmt.Println(arg)
+			word := strings.Fields(arg)
+			cmd := exec.Command(word[0], word[1:]...)
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stdout
+			err := cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+		/*cmd := exec.Command(args[0], args[1:]...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
 			fmt.Println(err)
-		}
+		}*/
 	}
 }
