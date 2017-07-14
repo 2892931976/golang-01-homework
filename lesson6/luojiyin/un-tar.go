edf9b218f5ce6f85d1cfee1cfd229849a4e151fe
luojiyin1987 <luojiyin@hotmail.com>
2017-07-14 10:49:03 +0800

    add untar

--- lesson6/luojiyin/un-tar.go
+++ lesson6/luojiyin/un-tar.go
@@ -1,32 +1,81 @@
 package main
 
 import (
-	"fmt"
 	"archive/tar"
 	"compress/gzip"
+	"flag"
+	"fmt"
 	"io"
-	"path/filepath"
+	"os"
+	"strings"
 )
 
-func  tar(src string, writers...io.Writer) error {
-	if _, err := os.Stat(src); err != nil {
-		return fmt.Errorf("nable to tar files -%v", err.Error()
+func main() {
+	flag.Parse()
+	sourcefile := flag.Arg(0)
+
+	if sourcefile == "" {
+		fmt.Println("Usage : need a sourcefile.tar ")
+		os.Exit(1)
+	}
+	file, err := os.Open(sourcefile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var fileReader io.ReadCloser = file
+
+	if strings.HasSuffix(sourcefile, ".gz") {
+		if fileReader, err = gzip.NewReader(file); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
-		mw := io.MultiWriter(writers...)
+		defer fileReader.Close()
+	}
+	tarBallReader := tar.NewReader(fileReader)
 
-		gzw := gzip.NewWriter(mw)
-		defer gzw.Close()
-
-		tw := tar.NewWriter(gzw)
-		defer  tw.Close()
-
-		return filepath.Walk(src, func(file string, info os.FileInfo, err error)error{
-			if err != nil {
-				return err
+	for {
+		header, err := tarBallReader.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
 			}
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
+		filename := header.Name
 
+		switch header.Typeflag {
+		case tar.TypeDir:
+			fmt.Println("Creating directory:", filename)
+			err = os.MkdirAll(filename, os.FileMode(header.Mode))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		case tar.TypeReg:
+			fmt.Println("Untarring:", filename)
+			writer, err := os.Create(filename)
 
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
 
+			}
+			io.Copy(writer, tarBallReader)
 
-} 
+			err = os.Chmod(filename, os.FileMode(header.Mode))
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+
+			}
+			writer.Close()
+		default:
+			fmt.Println("Unable to untar type: %c in file %s ", header.Typeflag, filename)
+		}
+	}
+}
