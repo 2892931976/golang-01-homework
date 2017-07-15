e7348b3098e74a6081780a34b61b6ada279264ae
kongsys <kongsys@163.com>
2017-07-15 17:22:36 +0800

    url parse demo

---
+++ lesson7/kongsys/uri.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "log"
+  "net/url"
+  "os"
+)
+
+func main() {
+  s := os.Args[1]
+  u, err := url.Parse(s)
+  if err != nil {
+    log.Fatal(err)
+  }
+  log.Println("scheme", u.Scheme)
+  log.Println("host", u.Host)
+  log.Println("path", u.Path)
+  log.Println("quertString", u.RawQuery)
+  log.Println("user", u.User)
+  log.Println("xx", u.Fragment)
+}
