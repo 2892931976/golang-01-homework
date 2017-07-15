ec5815b42f6e75f813baf658e3c7203a975720ff
kongsys <kongsys@163.com>
2017-07-15 14:56:42 +0800

    format join string by coroutine

--- lesson7/kongsys/join_str_coroutine4.go
+++ lesson7/kongsys/join_str_coroutine4.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func join_str(s []string, ch chan string) {
-  var temp string
-  for _, v := range s {
-    temp = fmt.Sprintf("%s %s", temp, v)
-  }
-  ch <- temp
+	var temp string
+	for _, v := range s {
+		temp = fmt.Sprintf("%s %s", temp, v)
+	}
+	ch <- temp
 }
 
 func main() {
-  s := []string{"hello", "golang", "c++", "world"}
+	s := []string{"hello", "golang", "c++", "world"}
 
-  c := make(chan string)
-  go join_str(s[:len(s)/2], c) 
-  go join_str(s[len(s)/2:], c) 
-  x, y := <-c, <-c
+	c := make(chan string)
+	go join_str(s[:len(s)/2], c)
+	go join_str(s[len(s)/2:], c)
+	x, y := <-c, <-c
 
-  fmt.Printf("%s %s", x, y)
+	fmt.Printf("%s %s", x, y)
 }
