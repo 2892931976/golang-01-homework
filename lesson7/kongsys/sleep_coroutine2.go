882c6ac71d4821b2879f7793ab7d7d2da538fdc7
kongsys <kongsys@163.com>
2017-07-15 14:28:58 +0800

    go format sleep coroutine sort

--- lesson7/kongsys/sleep_coroutine2.go
+++ lesson7/kongsys/sleep_coroutine2.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 )
 
 func main() {
-  s := []int{2, 7, 1, 6, 4}
-  for _, n := range s {
-    go func(n int)  {
-      time.Sleep(time.Duration(n) * time.Second) 
-      fmt.Println(n)
-    }(n)
-  }
-  time.Sleep(10 * time.Second)
+	s := []int{2, 7, 1, 6, 4}
+	for _, n := range s {
+		go func(n int) {
+			time.Sleep(time.Duration(n) * time.Second)
+			fmt.Println(n)
+		}(n)
+	}
+	time.Sleep(10 * time.Second)
 }
