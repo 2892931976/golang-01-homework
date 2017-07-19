fbfc677f53073942dc01fa1da87288fba3eab074
root <root@localhost.(none)>
2017-07-19 10:37:05 +0800

    .

--- lesson6/xuezhaomeng/mytar.go
+++ lesson6/xuezhaomeng/mytar.go
@@ -1,77 +1,63 @@
-package  main
+package main
 
 import (
-	"os"
-	"log"
-	"io"
-	"strings"
-	"compress/gzip"
 	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"os"
 	"path/filepath"
 )
-var  file_list []string
 
-func walkFunc(path string, info os.FileInfo, err error) error {
-	file_list = append(file_list, path)
+func tarFun(desc, src string) error {
+	fd, err := os.Create(desc) //创建目标文件
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	gw := gzip.NewWriter(fd) //写入.gz文件
+	defer gw.Close()
+
+	tr := tar.NewWriter(gw) //写入.tar文件
+	defer tr.Close()
+
+	err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error { //遍历src文件
+		fi, err := os.Stat(path) //获取包含时间戳和权限标志的os.FileInfo值,传递给FileInfoHeader
+		if err != nil {
+			return err
+		}
+
+		hdr, err := tar.FileInfoHeader(fi, "") //获取文件的头部信息
+		if err != nil {
+			return err
+		}
+		hdr.Name = path           //替换文件的Name信息 (使其包含之前的目录结构)
+		err = tr.WriteHeader(hdr) //写入文件的头部信息
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			fs, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer fs.Close()
+
+			if fi.Mode().IsRegular() {
+				io.Copy(tr, fs)
+			}
+		}
+		return nil
+	})
 	return nil
 }
 
-
-func  CreateTar(filename string,files []string) error {
-
-	f,err := os.Create(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	 defer  f.Close()
-
-	var fileWriter  io.WriteCloser = f
-	if  strings.HasPrefix(filename,".gz") {
-		fileWriter = gzip.NewWriter(f)
-		defer fileWriter.Close()
-	}
-	writer := tar.NewWriter(fileWriter)
-	defer  writer.Close()
-
-	for  _,name := range files {
-
-		if err := writeFileToTar(writer,name) ; err != nil  {
-			return  err
-		}
-
-
-	}
-	return  nil
-}
-
-func writeFileToTar(writer *tar.Writer,filename string) error {
-	f ,err := os.Open(filename)
-	if  err != nil  {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	stat ,err := f.Stat()
-	if  err != nil  {
-		log.Fatal(err)
-	}
-	header  := &tar.Header{
-		Name : filename,
-		Size : stat.Size(),
-	}
-	if err = writer.WriteHeader(header); err != nil {
-		return  err
-	}
-	fmt.Println(stat)
-	_,err = io.Copy(writer,f)
-	return  err
-}
-func main(){
+func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: mytar [desc] [src]")
 		return
 	}
-
-	filepath.Walk(os.Args[2], walkFunc)
-	CreateTar(os.Args[1], file_list)
+	tarFun(os.Args[1], os.Args[2])
 }
