b383c22c7bbaec44c21c37596fe8438a054841e7
root <root@localhost.(none)>
2017-07-20 17:47:06 +0800

    .

--- lesson7/xuezhaomeng/url.go
+++ lesson7/xuezhaomeng/url.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-
-	"io/ioutil"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -13,6 +15,51 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+func tarFun(desc, src string) error {
+	fd, err := os.Create(desc) //创建目标文件
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	gw := gzip.NewWriter(fd) //写入.gz文件
+	defer gw.Close()
+
+	tr := tar.NewWriter(gw) //写入.tar文件
+	defer tr.Close()
+
+	err = filepath.Walk(src, func(path string, info os.FileInfo, err error) error { //遍历src文件
+		fi, err := os.Stat(path) //获取包含时间戳和权限标志的os.FileInfo值,传递给FileInfoHeader
+		if err != nil {
+			return err
+		}
+
+		hdr, err := tar.FileInfoHeader(fi, "") //获取文件的头部信息
+		if err != nil {
+			return err
+		}
+		hdr.Name = path           //替换文件的Name信息 (使其包含之前的目录结构)
+		err = tr.WriteHeader(hdr) //写入文件的头部信息
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			fs, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer fs.Close()
+
+			if fi.Mode().IsRegular() {
+				io.Copy(tr, fs)
+			}
+		}
+		return nil
+	})
+	return nil
+}
 
 func CleanUrl(uri *url.URL, link string) string {
 	switch {
@@ -74,15 +121,15 @@ func Wget_Img(link, desc string) error {
 
 	imgname := filepath.Join(desc, u.Path)
 	dir := filepath.Dir(imgname) //获取父目录
-	os.MkdirAll(dir, 0755)
-	f, err := os.Create(imgname)
+	os.MkdirAll(dir, 0755)       //创建父目录
+	f, err := os.Create(imgname) //创建文件
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
 	body, _ := ioutil.ReadAll(img_resp.Body)
-	f.Write(body)
+	f.Write(body) //将文件写入
 	return nil
 
 }
@@ -102,5 +149,7 @@ func main() {
 		img_link := CleanUrl(access_url_info, link)
 		Wget_Img(img_link, desc)
 	}
+
+	tarFun(os.Args[1], desc)
 
 }
