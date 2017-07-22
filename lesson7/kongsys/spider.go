3e9cb3199c387d966875c22b0b7b2f9f84e4b68f
kongsys <kongsys@163.com>
2017-07-22 14:52:14 +0800

    add file, format code

--- lesson7/kongsys/spider.go
+++ lesson7/kongsys/spider.go
@@ -1,32 +1,154 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "github.com/PuerkitoBio/goquery"
+	"archive/tar"
+	"errors"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	urllib "net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 )
 
-func main() {
-  url := "http://daily.zhihu.com"
-  resp, err := http.Get(url)
-  if err != nil {
-    log.Fatal(err) 
-  }
-  defer resp.Body.Close()
-  if resp.StatusCode != http.StatusOK {
-    log.Fatal(resp.Status) 
-  }
-  doc, err := goquery.NewDocumentFromResponse(resp)
-  if err != nil {
-    log.Fatal(err) 
-  }
+func fetch(url string) ([]string, error) {
+	var urls []string
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	if err != nil {
+		return nil, err
+	}
 
-  doc.Find("img").Each(func(i int, s *goquery.Selection) {
-    link, ok := s.Attr("src") 
-    if ok {
-      log.Println(link) 
-    } else {
-      log.Println("src not found") 
-    }
-  })
+	doc.Find("img").Each(func(i int, s *goquery.Selection) {
+		link, ok := s.Attr("src")
+
+		if ok && len(link) > 0 {
+			urls = append(urls, link)
+		}
+	})
+	return urls, nil
+}
+func downloadImgs(urls []string, dir string) error {
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		go func(url string, dir string) {
+			defer wg.Done()
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			_, file := filepath.Split(url)
+			fname := filepath.Join(dir, file)
+			f, err := os.OpenFile(fname, os.O_CREATE|os.O_RDWR, 0644)
+			defer f.Close()
+			if err == nil {
+				f.Write(body)
+			}
+		}(url, dir)
+		wg.Add(1)
+	}
+	wg.Wait()
+	return nil
+}
+func CleanUrl(uri *urllib.URL, link string) string {
+	switch {
+	case strings.HasPrefix(link, "https") || strings.HasPrefix(link, "http"):
+		return link
+	case strings.HasPrefix(link, "//"):
+		return uri.Scheme + ":" + link
+	case strings.HasPrefix(link, "/"):
+		return fmt.Sprintf("%s://%s%s", uri.Scheme, uri.Host, link)
+	default:
+		p := strings.SplitAfter(uri.Path, "/")
+		path := strings.Join(p[:2], "")
+		return fmt.Sprintf("%s://%s%s%s", uri.Scheme, uri.Host, path, link)
+	}
+
+}
+func CleanUrls(u string, urls []string) []string {
+	var ret []string
+	uri, _ := urllib.Parse(u)
+	for i := range urls {
+		ret = append(ret, CleanUrl(uri, urls[i]))
+	}
+	return ret
+	return urls
+}
+
+func maketar(dir string, w io.Writer) error {
+	tr := tar.NewWriter(w)
+	defer tr.Close()
+	filepath.Walk(dir, func(name string, info os.FileInfo, err error) error {
+		header, err := tar.FileInfoHeader(info, "")
+		if err != nil {
+			return err
+		}
+		header.Name = name
+		fmt.Printf("name=%s, header.name=%s, info.name=%s\n", name, header.Name, info.Name())
+		err = tr.WriteHeader(header)
+		if err != nil {
+			return nil
+		}
+
+		if !info.IsDir() {
+			f, err := os.Open(name)
+			if err != nil {
+				return nil
+			}
+			defer f.Close()
+			_, err = io.Copy(tr, f)
+			return err
+		}
+		return nil
+	})
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "argument not enough")
+		os.Exit(1)
+	}
+	url := os.Args[1]
+	urls, err := fetch(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	urls = CleanUrls(url, urls)
+	for _, u := range urls {
+		log.Println(u)
+	}
+
+	tempdir, err := ioutil.TempDir(".", "spider")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tempdir)
+	err = downloadImgs(urls, tempdir)
+	if err != nil {
+		log.Panic(err)
+	}
+	f, err := os.OpenFile("sp.tar", os.O_CREATE|os.O_RDWR, 0644)
+	fmt.Println(tempdir)
+	maketar(tempdir, f)
+	f.Close()
 }
