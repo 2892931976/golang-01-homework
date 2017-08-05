7a9c13f1d4530828eeacddcb81f8b53aa119208c
kongsys <kongsys@163.com>
2017-08-05 15:43:39 +0800

    add tcp proxy

---
+++ lesson10/kongsys/proxy.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+  "net"
+  "flag"
+  "io"
+  "log"
+  "fmt"
+)
+
+var (
+  target = flag.String("target", "www.baidu.com", "target host")
+)
+
+func handleConn(conn net.Conn) {
+  var remote net.Conn
+  remoteaddr := fmt.Sprintf("%s:80", *target)
+  defer conn.Close()
+  remote, err := net.Dial("tcp", remoteaddr)
+  if err != nil {
+    log.Println(err) 
+    return
+  }
+  defer remote.Close()
+  go io.Copy(remote, conn)
+  io.Copy(conn, remote)
+}
+
+func main() {
+  listen, err := net.Listen("tcp", ":8021")
+  if err != nil {
+    log.Fatal(err) 
+  }
+
+  for {
+    conn, err := listen.Accept()
+    if err != nil {
+      log.Fatal(err) 
+    }
+    go handleConn(conn)
+  }
+}
