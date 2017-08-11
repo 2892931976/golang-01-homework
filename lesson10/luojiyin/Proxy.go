127e1c8684e92a9e88c9be2557c880ad831d7337
luojiyin <luojiyin@hotmail.com>
2017-08-11 23:21:50 +0800

    add tcp proxy

---
+++ lesson10/luojiyin/Proxy.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+var (
+	target = flag.String("target", "www.baidu.com", "target host")
+)
+
+func handleConn(conn net.Conn) {
+	var remote net.Conn
+	remoteaddr := fmt.Sprintf("%s:80", *target)
+	defer conn.Close()
+	remote, err := net.Dial("tcp", remoteaddr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer remote.Close()
+	go io.Copy(remote, conn)
+	io.Copy(conn, remote)
+}
+
+func main() {
+	fmt.Printf("%s", *target)
+	listen, err := net.Listen("tcp", ":8021")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+	}
+}
