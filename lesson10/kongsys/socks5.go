24001874b2a2a405d661277ad3c5f6a49ddc3059
kongsys <kongsys@163.com>
2017-08-11 14:02:37 +0800

    socks5.go supper domain ipv4

---
+++ lesson10/kongsys/socks5.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func readAddr(r *bufio.Reader) (string, error) {
+	version, _ := r.ReadByte()
+	log.Printf("readAddr version: %d", version)
+	if version != 5 {
+		return "", errors.New("bad Version")
+	}
+	cmd, _ := r.ReadByte()
+	switch cmd {
+	case 1:
+		log.Printf("readAddr cmd: %d, CONNECT", cmd)
+	case 2:
+		log.Printf("readAddr cmd: %d, BIND", cmd)
+	case 3:
+		log.Printf("readAddr cmd: %d, UDP", cmd)
+	default:
+		log.Printf("readAddr cmd: %d, unknow", cmd)
+		return "", errors.New("err cmd")
+	}
+
+	r.ReadByte()
+	atyp, _ := r.ReadByte()
+	var addrlen int
+	switch atyp {
+	case 1:
+		log.Printf("readAddr atyp: %d, ipV4", atyp)
+		addrlen = 4
+	case 3:
+		log.Printf("readAddr atyp: %d, domain", atyp)
+		addrl, _ := r.ReadByte()
+		fmt.Println(addrl)
+		addrlen = int(addrl)
+	case 4:
+		log.Printf("readAddr atyp: %d, ipV6", atyp)
+		addrlen = 16
+	}
+	host := make([]byte, addrlen)
+	var hh string
+	io.ReadFull(r, host)
+	fmt.Printf("host:%s\n", host)
+	switch atyp {
+	case 1:
+		hh = net.IPv4(host[0], host[1], host[2], host[3]).String()
+	case 3:
+		hh = string(host)
+		log.Printf("hh:%s", hh)
+	}
+	log.Printf("readAddr domain: %s", hh)
+
+	var port int16
+	binary.Read(r, binary.BigEndian, &port)
+
+	return fmt.Sprintf("%s:%d", hh, port), nil
+}
+
+func handshake(r *bufio.Reader, conn net.Conn) error {
+	version, _ := r.ReadByte()
+	log.Printf("version:%d", version)
+	if version != 5 {
+		return errors.New("bad Version")
+	}
+
+	nmethods, _ := r.ReadByte()
+	log.Printf("nmethods:%d", nmethods)
+
+	buf := make([]byte, nmethods)
+	io.ReadFull(r, buf)
+	log.Printf("%v", buf)
+
+	resp := []byte{5, 0}
+	log.Printf("resp %T\n", resp)
+	conn.Write(resp)
+	return nil
+}
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	r := bufio.NewReader(conn)
+	handshake(r, conn)
+	addr, _ := readAddr(r)
+	log.Printf("addr: %s", string(addr))
+	resp := []byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+	conn.Write(resp)
+	server, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer server.Close()
+	go io.Copy(server, conn)
+	io.Copy(conn, server)
+
+}
+
+func main() {
+	//flag.Parse()
+	l, err := net.Listen("tcp", ":8022")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, _ := l.Accept()
+		go handleConn(conn)
+	}
+}
