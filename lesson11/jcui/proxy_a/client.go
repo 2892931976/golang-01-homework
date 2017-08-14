de27814ce907c6177038805d6b3349a665198a6a
VincentJcui <cui6522123@gmail.com>
2017-08-14 15:31:55 +0800

    socks5  加密传输

---
+++ lesson11/jcui/proxy_a/client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"golang-01/lesson10/jcui/mycrypto"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	//这里的9999 指请求远端sevrer的端口
+	remote, err := net.Dial("tcp", ":9999")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer remote.Close()
+
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	key := "AB234asfds345safdasd"
+	// 将conn请求的数据加密后 通过io.Copy复制给remote
+	go func() {
+		defer wg.Done()
+		w := mycrypto.NewCryptoWriter(remote, key)
+		io.Copy(w, conn)
+	}()
+
+	// 将conn请求的数据加密后 通过io.Copy复制给remote
+	go func() {
+		defer wg.Done()
+		w := mycrypto.NewCryptoReader(remote, key)
+		io.Copy(conn, w)
+	}()
+	wg.Wait()
+}
+
+func main() {
+	//这里7777 指客户端启动监听的端口
+	listen, err := net.Listen("tcp", ":7777")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listen.Close()
+	for {
+		conn, _ := listen.Accept()
+		go handleConn(conn)
+		log.Print(conn.RemoteAddr().String())
+	}
+}
