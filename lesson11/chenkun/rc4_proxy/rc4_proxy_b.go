ead7ef2f787d5bf8d34b8e661102aba000c6340a
Administrator <chenkun1998@163.com>
2017-08-16 23:43:18 -0400

    add11

---
+++ lesson11/chenkun/rc4_proxy/rc4_proxy_b.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"rc4_proxy/encrypt_decrypt"
+	"sync"
+)
+
+var (
+	target = "127.0.0.1:8857" // socks5服务器的地址端口
+)
+
+func handleconn(clientconn net.Conn) {
+	key := "123456"
+	var serverconn net.Conn
+	var err error
+	serverconn, err = net.Dial("tcp", target)
+	if err != nil {
+		log.Print(err)
+		clientconn.Close()
+		return
+	}
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+	// go 读取clientconn数据，解密，发送到serverconn，直到clientconn的EOF，关闭serverconn
+	go func() {
+		defer wg.Done()
+		io.Copy(serverconn, cpt.NewCryptoReader(clientconn, key))
+		serverconn.Close()
+	}()
+	// go 读取serverconn数据，加密，发送到clientconn，直到serverconn的EOF，关闭clientconn
+	go func() {
+		defer wg.Done()
+		io.Copy(cpt.NewCryptoWriter(clientconn, key), serverconn)
+		clientconn.Close()
+	}()
+	// 等待两个协程结束
+	wg.Wait()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8856")
+	if err != nil {
+		log.Print(err)
+	}
+	defer listener.Close()
+
+	for {
+		clientconn, _ := listener.Accept()
+		go handleconn(clientconn)
+	}
+}
