612554d224f1824dd0b52ba841141056e5863606
kongsys <kongsys@163.com>
2017-08-18 18:00:46 +0800

    add mycrypto module

---
+++ lesson11/kongsys/mycrypto/mycrypto.go
@@ -0,0 +1,71 @@
+package mycrypto
+
+import (
+	"crypto/md5"
+	"crypto/rc4"
+	"io"
+)
+
+type CryptoWriter struct {
+	w      io.Writer
+	cipher *rc4.Cipher
+}
+
+func NewCryptoWriter(w io.Writer, key string) io.Writer {
+	md5sum := md5.Sum([]byte(key))
+	cipher, err := rc4.NewCipher(md5sum[:])
+	if err != nil {
+		panic(err)
+	}
+	return &CryptoWriter{
+		w:      w,
+		cipher: cipher,
+	}
+}
+
+func (w *CryptoWriter) Write(b []byte) (int, error) {
+	buf := make([]byte, len(b))
+	w.cipher.XORKeyStream(buf, b)
+	return w.w.Write(buf)
+}
+
+type CryptoReader struct {
+	r      io.Reader
+	cipher *rc4.Cipher
+}
+
+func NewCryptoReader(r io.Reader, key string) io.Reader {
+	md5sum := md5.Sum([]byte(key))
+	cipher, err := rc4.NewCipher(md5sum[:])
+	if err != nil {
+		panic(err)
+	}
+	return &CryptoReader{
+		r:      r,
+		cipher: cipher,
+	}
+}
+
+func (r *CryptoReader) Read(b []byte) (int, error) {
+	n, err := r.r.Read(b)
+	buf := b[:n]
+	r.cipher.XORKeyStream(buf, buf)
+	return n, err
+
+}
+
+/*
+func main() {
+  remote, err := net.Dial("tcp", "127.0.0.1:8081")
+  if err != nil {
+    panic(err)
+  }
+  w := NewCryptoWriter(remote, "123456")
+  w.Write([]byte("hello"))
+
+  r := NewCryptoReader(remote, "123456")
+  buf := make([]byte, 1024)
+  r.Read(buf)
+  fmt.Println(string(buf))
+}
+*/
