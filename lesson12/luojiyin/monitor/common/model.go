321b6ad8e12dd5507d04df6b27725f1b974375cd
luojiyin <luojiyin@hotmail.com>
2017-08-22 22:46:32 +0800

    add monitor package

--- lesson12/luojiyin/monitor/common/model.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type Metric struct {
-	Metric    string   `json:"metric"`
-	Endpoint  string   `json:"endpoint"`
-	Tag       []string `json:"tag"`
-	Value     float64  `json:"value"`
-	Timestamp int64    `json:"timestap"`
-}
---
+++ lesson12/luojiyin/monitor/sched.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"time"
+
+	"github.com/51reboot/golang-01-homework/lesson12/luojiyin/common"
+)
+
+type MetricFunc func() []*common.Metric
+
+type Sched struct {
+	ch chan *common.Metric
+}
+
+func NewSched(ch chan *common.Metric) *Sched {
+	return &Sched{
+		ch: ch,
+	}
+}
+
+func (s *Sched) AddMetric(collecter MetricFunc, step time.Duration) {
+	ticker := time.NewTicker(step)
+	for range ticker.C {
+		for _, m := range collecter() {
+			if m != nil {
+				s.ch <- m
+			}
+		}
+	}
+}
